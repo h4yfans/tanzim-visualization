a81acc6fde2e76a7c3e98bcfe4abba28ea9ae100
Kaan Karaca <kaan94karaca@gmail.com>
2019-04-07 23:51:26 +0300

    @wip

--- src/chart/views.py
+++ src/chart/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
+from django.db.models.functions import Cast
+from django.db.models import CharField, DateField
+
+from .models import Vegetable, Product
 
 
 class IndexView(TemplateView):
@@ -6,3 +10,8 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+
+        context['dates'] = list(
+            Product.objects.annotate(ndate=Cast('date', CharField())).order_by('date').values_list('ndate', flat=True))
+        context['prices'] = list(Product.objects.order_by('date').values_list('price', flat=True))
+        return context
