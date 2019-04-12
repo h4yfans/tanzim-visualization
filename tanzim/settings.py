f1c3fc75bffc0284599aa3b701030fa40edea09d
Kaan Karaca <kaan94karaca@gmail.com>
2019-04-12 22:46:46 +0300

    settings

--- tanzim/settings.py
+++ tanzim/settings.py
@@ -19,11 +19,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "2q7%60lnpdbq)qpm4!+gcq=plmacwx0c4eci15#l5qjfh$l%bv"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
+ALLOWED_HOSTS = ['tanzimapp.herokuapp.com']
 
 # Application definition
 
