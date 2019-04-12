f3ed03d70b52b5e12dbaf43813b596d66d0a895a
Kaan Karaca <kaan94karaca@gmail.com>
2019-04-12 22:35:07 +0300

    files are moved

--- src/chart/api/routers.py
+++
@@ -1,7 +0,0 @@
-from chart.api.viewsets import VegetableViewSet, ProductViewSet
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register('vegetables', VegetableViewSet, base_name='vegetable')
-router.register('products', ProductViewSet, base_name='products')
-
